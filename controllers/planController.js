17f9deee6033836ceec28f6b83b237502e32a730
Lstanislao <luis.stanislao@correo.unimet.edu.ve>
2021-05-25 16:09:53 -0400

    hay fe

--- controllers/planController.js
+++ controllers/planController.js
@@ -2,35 +2,29 @@ const { response } = require("express");
 const Plan = require("../models/plan");
 
 /**
- * 
+ *
  * @description: Busca los planes activos para los proyectos
  *
  */
 const getPlanes = async (req, res = response) => {
   try {
+    const planes = await Plan.find({ active: true });
 
-    const planes = await Plan.find(
-        { active: true  }
-      )
-    
-    console.log(planes)
+    console.log(planes);
 
-  res.json({
+    res.json({
       ok: true,
       data: planes,
-      
     });
-
   } catch (error) {
     console.log(error);
     res.status(500).json({
       ok: false,
-      msg: 'La peticion de buscar planes fallo'
-    })
+      msg: "La peticion de buscar planes fallo",
+    });
   }
-}
-
+};
 
 module.exports = {
-  getPlanes
-}
+  getPlanes,
+};
