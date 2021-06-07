39549aba0f66e11a1b932dc101f41b7489abc3eb
Lstanislao <luis.stanislao@correo.unimet.edu.ve>
2021-06-07 13:43:32 -0400

    dejame salir

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -2,7 +2,7 @@ const { response } = require("express");
 const Proyecto = require("../models/proyecto");
 const Usuario = require("../models/usuario");
 const Plan = require("../models/plan");
-const { getUsuarioByEmail } = require('../controllers/authController'); 
+const { getUsuarioByEmail } = require("../controllers/authController");
 
 /**
  * @description: crea un nuevo proyecto , se crea por default activo y no archivado
@@ -11,7 +11,7 @@ const { getUsuarioByEmail } = require('../controllers/authController');
 const createProyecto = async (req, res = response) => {
   try {
     console.log(req.body);
-    
+
     const proyecto = new Proyecto(req.body);
 
     proyecto.save();
@@ -19,17 +19,15 @@ const createProyecto = async (req, res = response) => {
     res.json({
       ok: true,
       data: proyecto,
-      
     });
-
   } catch (error) {
     console.log(error);
     res.status(500).json({
       ok: false,
-      msg: 'La peticion de crear proyecto fallo'
-    })
+      msg: "La peticion de crear proyecto fallo",
+    });
   }
-}
+};
 
 /**
  * @description: Actualiza un proyecto. Sirve tanto para los campos que el usuario cambie como
@@ -38,106 +36,101 @@ const createProyecto = async (req, res = response) => {
  */
 const updateProyecto = async (req, res = response) => {
   try {
-   
-    const { id_proyecto } =  req.body;
+    const { id_proyecto } = req.body;
 
     const proyecto = new Proyecto(req.body);
 
-    Proyecto.findByIdAndUpdate(id_proyecto ,{...req.body},
+    Proyecto.findByIdAndUpdate(
+      id_proyecto,
+      { ...req.body },
       function (err, docs) {
-        if (err){
-            console.log(err)
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Actualizado el proyecto", docs);
         }
-        else{
-            console.log("Actualizado el proyecto", docs);
-        }
-      })
+      }
+    );
 
-  res.json({
+    res.json({
       ok: true,
       data: proyecto,
-      
     });
-
   } catch (error) {
     console.log(error);
     res.status(500).json({
       ok: false,
-      msg: 'La peticion de actualizar proyecto fallo'
-    })
+      msg: "La peticion de actualizar proyecto fallo",
+    });
   }
-}
+};
 
 /**
- * 
- * @description: Busca un proyecto con su respectivo plan y miembros 
- * 
+ *
+ * @description: Busca un proyecto con su respectivo plan y miembros
+ *
  * @param: id del proyecto
  */
 const getProyecto = async (req, res = response) => {
   try {
-    console.log(req.params.proyecto)
-    console.log(req.body)
-    const  uid  = req.params.proyecto;
+    console.log(req.params.proyecto);
+    console.log(req.body);
+    const uid = req.params.proyecto;
 
     const proyecto = await Proyecto.findById(uid)
-      .populate('id_plan')
-      .populate('miembros');
-    
-    console.log(proyecto)
+      .populate("id_plan")
+      .populate("miembros");
 
-  res.json({
+    console.log(proyecto);
+
+    res.json({
       ok: true,
       data: proyecto,
-      
     });
-
   } catch (error) {
     console.log(error);
     res.status(500).json({
       ok: false,
-      msg: 'La peticion de buscar proyecto fallo'
-    })
+      msg: "La peticion de buscar proyecto fallo",
+    });
   }
-}
+};
 
 /**
- * 
- * @description: Busca un los proyectos activos de un usuario 
- * 
+ *
+ * @description: Busca un los proyectos activos de un usuario
+ *
  * @param: id del usuario
  */
 const getProyectosUsuario = async (req, res = response) => {
   try {
-    console.log(req.params.user)
-    console.log(req.body)
-    const  uid  = req.params.user;
+    console.log(req.params.user);
+    console.log(req.body);
+    const uid = req.params.user;
 
-    const proyectos = await Proyecto.find(
-        { miembros: { $in: [uid]}, active: true  }
-      ).sort( { createdAt: -1} )
-    
-    console.log(proyectos)
+    const proyectos = await Proyecto.find({
+      miembros: { $in: [uid] },
+      active: true,
+    }).sort({ createdAt: -1 });
 
-  res.json({
+    console.log(proyectos);
+
+    res.json({
       ok: true,
       data: proyectos,
-      
     });
-
   } catch (error) {
     console.log(error);
     res.status(500).json({
       ok: false,
-      msg: 'La peticion de buscar proyectos fallo'
-    })
+      msg: "La peticion de buscar proyectos fallo",
+    });
   }
-}
-
+};
 
 module.exports = {
   createProyecto,
   updateProyecto,
   getProyectosUsuario,
-  getProyecto
-}
+  getProyecto,
+};
